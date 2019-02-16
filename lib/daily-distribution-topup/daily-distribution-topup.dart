7fc4fffd0a0d85bee3f5afe3e3fd51d19bcc1c69
HP <pchea99@gmail.com>
2019-02-16 16:48:35 +0700

    create model dto.

--- lib/daily-distribution-topup/daily-distribution-topup.dart
+++ lib/daily-distribution-topup/daily-distribution-topup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sm_app/res/font-size-res.dart';
 import 'package:sm_app/res/string-res.dart';
+import 'package:sm_app/utils/app-bar.dart';
 
 class DailyDistributionTopUp extends StatefulWidget {
   @override
@@ -38,9 +39,8 @@ class _DailyDistributionTopUpState extends State<DailyDistributionTopUp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container()
+    return AppBarUtil(
+      title: StringRes.dailyDistributionTopUp,
     );
   }
 }
--- lib/utils/app-bar.dart
+++ lib/utils/app-bar.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class AppBarUtil extends StatefulWidget {
   final String title;
   final List<Widget> actions;
+  final Widget body;
 
   AppBarUtil({
     this.title,
-    this.actions
+    this.actions,
+    this.body
   });
 
   @override
@@ -17,15 +19,20 @@ class _AppBarState extends State<AppBar> {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: widget.title,
-      actions: widget.actions,
-      leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          onPressed: onPressed
+    return Scaffold(
+      appBar: AppBar(
+        title: widget.title,
+        actions: widget.actions,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: onPressed
+        ),
+      ),
+      body: Container(
+        child: widget.body,
       ),
     );
   }
@@ -34,3 +41,4 @@ class _AppBarState extends State<AppBar> {
     Navigator.pop(context);
   }
 }
+
